7c5600f1b1d611b864de1cbbb0bf7069edacea8d
samanthajoypoteet <111479932+samanthajoypoteet@users.noreply.github.com>
2022-11-07 14:11:49 -0700

    Update NHFL_Florida.R

--- code/NHFL_Florida.R
+++ code/NHFL_Florida.R
@@ -1,44 +1,64 @@
-#I know hard coding working directories is very bad but until we can ge larger files on github I want to remember were our data is organized. i am sorry.
-
-#pacman::p_load(sf, readr, raster,tidyr,tidyverse, dplyr, lubridate,haven, stringr, readxl, tigris,ggplot2, tmap)
-#pacman::p_load(sf, readr,tidyverse, dplyr, haven, tigris, ggplot2, tmap)
 library(sf)
 library(readr)
 library(tidyverse)
 library(dplyr)
 library(tigris)
 library(ggplot2)
+library(tmap)
 
+#### LOAD IN DATA ##### -------------------------------------------------------------
+# load in flood hazard data layer for Broward County, FL
+broward_hazzard <- st_read("data_raw/12011C_20221102/S_FLD_HAZ_AR.shp") # CRS NAD83
+broward_hazzard <- st_make_valid(broward_hazzard)
 
-#load in florida flood hazard data layer
-setwd("/Users/m28t112/OneDrive - Montana State University/Documents")
-NFHL = "NFHL_12_20220922.gdb"
+# load in clean Florida demographics (tract income and race data)
+florida_pop <- read_csv("data_clean/Florida_Race_and_Income_tract")
 
-st_layers(NFHL)
+# load in US county geometry and florida tracts
+counties <- counties(cb = TRUE)
+tracts <- tracts(cb = TRUE)%>%
+  filter(STUSPS == "FL")
 
-hazard<-st_read(dsn = NFHL, layer = "S_FLD_HAZ_AR") 
+#### CLEAN #### ---------------------------------------------------------------
 
-setwd("/Users/m28t112/OneDrive - Montana State University/Documents/GitHub/DataAnalytics_TermProject")
-#load in clean florida tract income and race data
-florida_tract <- read_csv("/data/clean/Florida_Race_and_Income_tract")
+# subset demographics to only Broward County
+broward_pop <- florida_pop%>%
+  filter(COUNTY == "Broward County")
 
-counties<-counties(cb=TRUE)
-
-florida_geometry<-counties%>%
+# subset counties data to create Broward County outline
+florida_geometry <- counties%>%
   filter(STUSPS == "FL")%>%
   rename("COUNTY" = "NAMELSAD")
 
-#add geometry to florida census tract data
-merge<- left_join(florida_tract, florida_geometry, by = "COUNTY")
-
-merge<-st_as_sf(merge)
+broward_outline <- florida_geometry%>%
+  filter(COUNTY == "Broward County")
 
 
-#join tract data and hazard layer
-sf::sf_use_s2(FALSE)
-florida_hazard_tract<-st_join(hazard, merge)
-write_csv(florida_hazard_tract, "/data/clean/fl_hazard_tract_merge.csv")
-st_write(florida_hazard_tract, "/data/clean/fl_hazard_tract_merge.shp")
+# subset tracts data to Create tract level geometry for Broward County
+broward_geo <- tracts%>%
+  filter(NAMELSADCO == "Broward County")
 
-florida_hazard_tract_intersects<-st_intersects(hazard, merge)
+# rename so boward_geo and demograpics can be joined
+broward_geo1 <- broward_geo%>%
+  rename("TRACTA" = "TRACTCE")
 
+broward_pop1 <- broward_pop%>%
+  filter(TRACTA == 990000)
+
+
+#### JOINS / INTERSECTIONS #### -----------------------------------------------------------------
+# join broward demographics with broward tract geometry
+broward_pop_geo <- left_join(broward_pop1, broward_geo1, by = "TRACTA")
+broward_pop_geo <-st_as_sf(broward_pop_geo)
+
+# spatial join with population and the hazzard layer
+broward_pop_hazzard_join <- st_join(broward_hazzard, broward_pop_geo)
+
+# intersects with population and the hazzard layer
+broward_pop_hazzard_intersects <- st_intersects(broward_hazzard, broward_pop_geo)
+broward_pop_hazzard_intersects1 <- st_intersects(broward_pop_geo, broward_hazzard) #OUTPUT LIST OF 1 :(
+
+# current issue with st_join and st_intersects is the y dataset comes up as NA
+
+# intersections
+broward_pop_hazzard_intersection <- st_intersection(broward_hazzard, broward_pop_geo) # ouput 0 obs :(
