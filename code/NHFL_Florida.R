d38a5041a7d7cbb82d3b0e0e34d19a5785809d84
samanthajoypoteet <111479932+samanthajoypoteet@users.noreply.github.com>
2022-11-07 20:27:23 -0700

    created maps
    Subjectively-ish ranked the severity of risk for different flood hazard risk zones.

--- code/NHFL_Florida.R
+++ code/NHFL_Florida.R
@@ -1,3 +1,5 @@
+# set working directory to shared OneDrive
+
 library(sf)
 library(readr)
 library(tidyverse)
@@ -5,6 +7,7 @@ library(dplyr)
 library(tigris)
 library(ggplot2)
 library(tmap)
+library(car)
 
 #### LOAD IN DATA ##### -------------------------------------------------------------
 # load in flood hazard data layer for Broward County, FL
@@ -43,7 +46,12 @@ broward_geo1 <- broward_geo%>%
   rename("TRACTA" = "TRACTCE")
 
 broward_pop1 <- broward_pop%>%
-  filter(TRACTA == 990000)
+  filter(TRACTA != 990000)
+
+# create numeric variable that ranks hazzard layer 
+broward_hazzard1 <- broward_hazzard%>%
+  mutate(risk = recode(broward_hazzard$FLD_ZONE,
+                       recodes = "'VE' = 5; 'AE' = 4; 'AH'= 3; 'AO' = 2; 'X' = 1; 'D' = 0; 'AREA NOT INCLUDED' = 0;"))
 
 
 #### JOINS / INTERSECTIONS #### -----------------------------------------------------------------
@@ -62,3 +70,32 @@ broward_pop_hazzard_intersects1 <- st_intersects(broward_pop_geo, broward_hazzar
 
 # intersections
 broward_pop_hazzard_intersection <- st_intersection(broward_hazzard, broward_pop_geo) # ouput 0 obs :(
+
+
+#### MAPPING ####--------------------------------------------------------------------------------
+
+income <- tm_shape(broward_pop_geo) + tm_polygons("median_household_income" , 
+                                                              style = "cont",
+                                                              breaks = c(14000, 50000, 100000, 150000, 200000),
+                                                              colorNA = "light grey",
+                                                              title = "Median Household Income",
+                                                              palette = "YlGn")
+income <- income + tm_shape(broward_geo1) + tm_borders(lwd = 0.5, col = "black")
+income
+
+hazzard <- tm_shape(broward_hazzard1) + tm_polygons("risk",
+                                                   style = "cont",
+                                                   breaks = c(0, 1, 2, 3, 4, 5),
+                                                   border.alpha = 0,
+                                                   title = "Flood Hazzard Zones",
+                                                   palette = "PuRd")
+hazzard <- hazzard + tm_shape(broward_geo1) + tm_borders(lwd = 0.5, col = "black")
+hazzard
+
+panel <- tmap_arrange(income, hazzard, ncol = 2)
+panel
+
+# change working directory here to save to github
+tmap_save(income, "output/maps/broward_median_income.png")
+tmap_save(hazzard, "output/maps/broward_ranked_fld_hzd.png")
+tmap_save(panel, "output/maps/broward_panel_income_fldhzd.png")
